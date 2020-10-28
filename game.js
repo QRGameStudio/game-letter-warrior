1d0811eba81a48240bf4512cf079b3461bc9e723
Adam Hlaváček <git@adamhlavacek.com>
2020-10-28 17:38:58 +0100

    feat: cutting objects

--- game.js
+++ game.js
@@ -3,6 +3,10 @@ let canvasWidth;
 let canvasHeight;
 let ctx = canvas ? canvas.canvas.getContext('2d') : null;
 let objects = [];
+let lastMousePoint = null;
+const abs = Math.abs;
+const min = Math.min;
+const sign = Math.sign;
 
 /**
  * All coordinates are relative to the size of the viewport
@@ -22,10 +26,14 @@ function draw() {
     window.requestAnimationFrame(draw);
 }
 
-function MousePoint(x, y) {
+function MousePoint(x, y, c=null) {
     let alpha = 1;
+    this.m = true; // is mouse event
+    this.x = x;
+    this.y = y;
+
     this.draw = () => {
-        ctx.fillStyle = '#fff';
+        ctx.fillStyle = c ? c : '#fff';
         ctx.globalAlpha = alpha;
         ctx.beginPath();
         ctx.arc(X(x), Y(y), 5, 0, Math.PI * 2);
@@ -38,15 +46,37 @@ function MousePoint(x, y) {
 
 function GameObject(content, x, y, speedX, speedY) {
     let gravity = 0.05;
-    this.draw = () => {
+    let hit = false;
+    this.m = false; // is NOT mouse event
+    this.draw = (i) => {
+        const size = objectSize();
         ctx.globalAlpha = 1;
-        ctx.fillStyle = '#fff';
-        ctx.font = `${canvasHeight/10}px monospace`;
+        ctx.font = `${size}px monospace`;
+        if (!hit) {
+            const sizeX = ctx.measureText(content).width;
+            const sizeQuarter = min(size, sizeX) / 4;
+            const centerX = (X(x) + sizeX / 2);
+            const centerY = (Y(y) - size / 2);
+            const cross = objects.slice(i).find(
+                (o) => o.m && abs(centerX - X(o.x)) < sizeQuarter && abs(centerY - Y(o.y)) < sizeQuarter
+            );
+            if (cross) {
+                hit = true;
+                speedX = 0;
+                speedY = 0;
+            }
+            ctx.globalAlpha = 1;
+            ctx.fillStyle = '#0f0';
+            ctx.beginPath();
+            ctx.arc(centerX, centerY, 5, 0, Math.PI * 2);
+            ctx.fill();
+        }
+        ctx.fillStyle = hit ? '#f00' : '#fff';
         ctx.fillText(content, X(x), Y(y));
         x += speedX;
         y += speedY;
         speedY += gravity;
-        return y < canvasHeight;
+        return y < 100;
     }
     objects.push(this);
 }
@@ -56,13 +86,51 @@ function gameEntryPoint() {
    if (!ctx) {
        ctx = canvas.getContext('2d');
    }
-   canvas.onmousemove = (e) => new MousePoint(relX(e.x), relY(e.y));
-   canvas.ontouchmove = (e) => new MousePoint(relX(e.touches[0].clientX), relY(e.touches[0].clientY));
-   canvas.onclick = (e) => new GameObject('X', relX(e.clientX), relY(e.clientY), relX(5), relY(-5));
-   setInterval(() => {
-       new GameObject('@', 50, 99, random(-0.6, 0.6), random(-1.5, -4));
-   }, 500)
+   canvas.onmousemove = (e) => addMousePoint(e.x, e.y);
+   canvas.ontouchmove = (e) => addMousePoint(e.touches[0].clientX, e.touches[0].clientY);
+   canvas.onmouseleave = () => lastMousePoint = null;
+   canvas.ontouchend = () => lastMousePoint = null;
+   // canvas.onclick = () => launchObject();
+   setInterval(() => launchObject(), 500);
    draw();
+}
+
+function addMousePoint(x, y) {
+    const newMousePoint = new MousePoint(relX(x), relY(y));
+
+    if (lastMousePoint) {
+        const sizeQ = relY(objectSize() / 8);
+        let mousePoint = newMousePoint;
+        while (true) {
+            let nx = null;
+            let ny = null;
+            const dx = mousePoint.x - lastMousePoint.x;
+            const dy = mousePoint.y - lastMousePoint.y;
+
+            if (abs(dx) > sizeQ) {
+                nx = mousePoint.x - sign(dx) * sizeQ;
+            }
+            if (abs(dy) > sizeQ) {
+                ny = mousePoint.y - sign(dy) * sizeQ;
+            }
+
+            if (ny === null || nx === null) {
+                break;
+            }
+            console.log('adding');
+            mousePoint = new MousePoint(nx ? nx : mousePoint.x, ny ? ny : mousePoint.y, '#00f');
+        }
+    }
+
+    lastMousePoint = newMousePoint;
+}
+
+function objectSize() {
+    return Y(10);
+}
+
+function launchObject() {
+    new GameObject('X', 50, 99, random(-0.6, 0.6), random(-1.5, -4));
 }
 
 function random(from, to) {
